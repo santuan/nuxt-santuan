0d11b71b249f9ee23c290133d436f6a1dd19629c
stn <smrcouto88@gmail.com>
2022-11-21 12:04:38 -0300

    zag modal

--- components/Lab/DalleGrid.vue
+++ components/Lab/DalleGrid.vue
@@ -3,54 +3,54 @@
     class="grid grid-cols-2 gap-1 p-1 from-yellow-400 bg-gradient-to-b via-yellow-500 to-gray-900 scroll-mt-24 md:grid-cols-3"
     id="ejemplo4"
   >
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Pintura de un rey conejo"
       img="https://res.cloudinary.com/srcouto/image/upload/q_auto:eco/v1654748378/santuan/history/01_13_spdx0t.webp"
     />
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Un congreso de gatos votando meow"
       img="https://res.cloudinary.com/srcouto/image/upload/q_auto:eco/v1654747690/santuan/history/01_0_tes00q.webp"
     />
 
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Render - Un oceano repleto de barquitos bajo una constelación de galaxias"
       img="https://res.cloudinary.com/srcouto/image/upload/q_auto:eco/v1654747810/santuan/history/01_19_jh8ikd.jpg"
     />
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Una guitarra con una planta creciendo en su interior"
       img="https://res.cloudinary.com/srcouto/image/upload/c_scale,h_1024,q_auto:eco/v1654748288/santuan/history/01_75_giaebn.jpg"
     />
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Pintura de Una ciudad de tardenoche con una luna brillando como un sol"
       img="https://res.cloudinary.com/srcouto/image/upload/c_scale,h_1024,q_auto:eco/v1654748221/santuan/history/01_79_bewrli.webp"
     />
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Arte digital de una niña sobre una casa en el arbol."
       img="https://res.cloudinary.com/srcouto/image/upload/q_auto:eco/v1654747800/santuan/history/ImageSequence032_mkwbse.jpg"
     />
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Un conejo escuchando la radio - arte digital "
       img="https://res.cloudinary.com/srcouto/image/upload/q_auto:eco/v1654747651/santuan/history/01_4_fitcxc.webp"
     />
 
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Arte digital de una habitacion con una ventana gigante con una persona flotando en una nube"
       img="https://res.cloudinary.com/srcouto/image/upload/q_auto:eco/v1654747878/santuan/history/ImageSequence030_wupe68.webp"
     />
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Arte digital de una habitacion con una ventana gigante con una persona flotando en una nube"
       img="https://res.cloudinary.com/srcouto/image/upload/q_auto:eco/v1654748340/santuan/history/004_6_oox3dv.webp"
     />
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Arte digital de una habitacion con una ventana gigante con una persona flotando en una nube"
       img="https://res.cloudinary.com/srcouto/image/upload/q_auto:eco/v1654747903/santuan/history/03_2_tur5ci.webp"
     />
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Arte digital de una habitacion con una ventana gigante con una persona flotando en una nube"
       img="https://res.cloudinary.com/srcouto/image/upload/q_auto:eco/v1654747866/santuan/history/01_49_blcwpq.webp"
     />
 
-    <LabModalImage
+    <LabModalImageDialogImageExpand
       title="Arte digital de una habitacion con una ventana gigante con una persona flotando en una nube"
       img="https://res.cloudinary.com/srcouto/image/upload/q_auto:eco/v1654748282/santuan/history/01_24_atkuzw.webp"
     />
